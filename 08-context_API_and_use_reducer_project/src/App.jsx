55862c3e7d83f04b2181f6134011caa9f595c607
Iuri Severo <nobreiuri@gmail.com>
2024-09-22 12:15:53 -0300

    Create cart context and add functions to update and add items to the cart

--- 08-context_API_and_use_reducer_project/src/App.jsx
+++ 08-context_API_and_use_reducer_project/src/App.jsx
@@ -5,6 +5,8 @@ import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
 
+import { CartContext } from "./store/shopping-cart-context.jsx";
+
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
     items: [],
@@ -66,20 +68,25 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+    updateCartItemQuantity: handleUpdateCartItemQuantity
+  };
+
   return (
-    <>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
+            <Product {...product}/>
           </li>
         ))}
       </Shop>
-    </>
+    </CartContext.Provider>
   );
 }
 
